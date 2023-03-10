4a5460e54015129b43ec7e599d22e44ddd39a5ac
TranNgocVy <ngocvy04042001@gmail.com>
2023-03-10 21:23:16 +0700

    Algorithm

---
+++ lib/Algorithm/Algorithm.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void miniMaxSum(List<int> arr){
+  if(arr.length == 5){
+    int sum = 0;
+    int max = arr[0];
+    int min = arr[0];
+
+    arr.forEach((element) {
+      sum+= element;
+
+      if(max < element){
+        max = element;
+      }else{
+        if(min > element){
+          min = element;
+        }
+      }
+    });
+    print("${sum - max} ${sum - min}");
+  }
+  else{
+    print("0 0");
+  }
+
+}
+void main() {
+  List<int> arr = [12,5,8,3,10];
+  miniMaxSum(arr);
+}
